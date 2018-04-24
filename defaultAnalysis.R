4905832a69c1bdd43e80fad08610af743b0e3d60
A Naranjo <anara004@gmail.com>
2018-04-23 19:40:19 -0500

    added regsubsets and more cleaning

--- defaultAnalysis.R
+++ defaultAnalysis.R
@@ -2,70 +2,105 @@ library(tidyverse)
 library(modelr)
 library(car)
 library(lubridate)
+library(leaps)
 library(class)
 library(gbm)
+library(gridExtra)
 
 # TODO:
 # 1. Figure out error for linear model
 # 2. Cross-validation for linear and lda models
 # 3. Implement KNN
 # 4. Figure out performance issue with Boosting 
-
+# 5. Try using Regsubsets for Linear Models (start with all predictors)
+# 6. PCA?
 
 ######################
 # Import data
 
 # df_2005 <- read_csv('data/Loans_20050101to20130101_20180415T060005.csv') # Has problems (753 rows)
-df_2013 <- read_csv('data/Loans_20130101to20140101_20180415T060158.csv')
-df_2014 <- read_csv('data/Loans_20140101to20150101_20180415T060226.csv')
-df_2015 <- read_csv('data/Loans_20150101to20160101_20180415T060359.csv')
-# df_2016 <- read_csv('data/Loans_20160101to20170101_20180415T060742.csv')
-df_2017 <- read_csv('data/Loans_20170101to20180101_20180415T060911.csv')
-df_2018 <- read_csv('data/Loans_20180101toCurrent_20180415T061114.csv')
+df_2013 <- read_csv('data/Loans_20130101to20140101_20180415T060158.csv') %>% 
+  mutate(lost_money = factor(ifelse(loan_status_description == 'COMPLETED', 1, 0))
+         ,prosper_rating = factor(prosper_rating)
+         ,term = factor(term)
+         ,year = '2013')
 
-# df_all_years <- bind_rows(df_2013, df_2014, df_2015, df_2017, df_2018) 
+df_2014 <- read_csv('data/Loans_20140101to20150101_20180415T060226.csv') %>% 
+  mutate(lost_money = factor(ifelse(loan_status_description == 'COMPLETED', 1, 0))
+         ,prosper_rating = factor(prosper_rating)
+         ,term = factor(term)
+         ,year = '2014')
+
+df_2015 <- read_csv('data/Loans_20150101to20160101_20180415T060359.csv') %>% 
+  mutate(lost_money = factor(ifelse(loan_status_description == 'COMPLETED', 1, 0))
+         ,prosper_rating = factor(prosper_rating)
+         ,term = factor(term)
+         ,year = '2015')
+
+df_2016 <- read_csv('data/Loans_20160101to20170101_20180415T060742.csv') %>%
+mutate(lost_money = factor(ifelse(loan_status_description == 'COMPLETED', 1, 0))
+       ,prosper_rating = factor(prosper_rating)
+       ,term = factor(term)
+       ,year = '2016')
+# warnings(df_2016)
+
+df_2017 <- read_csv('data/Loans_20170101to20180101_20180415T060911.csv') %>% 
+  mutate(lost_money = factor(ifelse(loan_status_description == 'COMPLETED', 1, 0))
+         ,prosper_rating = factor(prosper_rating)
+         ,term = factor(term)
+         ,year = '2017')
+
+df_2018 <- read_csv('data/Loans_20180101toCurrent_20180415T061114.csv') %>% 
+  mutate(lost_money = factor(ifelse(loan_status_description == 'COMPLETED', 1, 0))
+         ,prosper_rating = factor(prosper_rating)
+         ,term = factor(term)
+         ,year = '2018')
+
+df_all_years <- bind_rows(df_2013, df_2014, df_2015, df_2016, df_2017)
 
 
 ######################
 # Clean data
 
+# Remove redundant or non-useful columns
+df_all_years <- df_all_years %>%
+  select(-loan_default_reason, -loan_number, -days_past_due, -debt_sale_proceeds_received, -loan_status,
+         -loan_default_reason_description, -loan_status_description, -next_payment_due_date, -principal_paid,
+         -origination_date, -interest_paid, -borrower_rate, -year)
+
 # Remove loans which are currently outstanding or were cancelled
-df_2014 <- df_2014 %>% 
+df_all_years <- df_all_years %>% 
   filter(!loan_status_description %in% c('CURRENT', 'CANCELLED'))
 
 # Remove loans with no rating
-df_2014 <- df_2014 %>% 
+df_all_years <- df_all_years %>% 
   filter(prosper_rating != 'N/A')
 
 # # Extract month from origination date
-# df_2014 <- df_2014 %>%
+# df_all_years <- df_all_years %>%
 #   mutate(month_of_origination = factor(month(origination_date))) 
 # Creates "aliased coefficients" according when trying I try to run vif
 
 # Change datatypes of select columns to factor
-df_2014 <- df_2014 %>% 
+df_all_years <- df_all_years %>% 
   mutate(lost_money = factor(ifelse(loan_status_description == 'COMPLETED', 1, 0))
          ,prosper_rating = factor(prosper_rating)
          ,term = factor(term))
 
-# Remove redundant or non-useful columns
-df_2014 <- df_2014 %>% 
-  select(-loan_default_reason, -loan_number, -days_past_due, -debt_sale_proceeds_received, -loan_status, 
-         -loan_default_reason_description, -loan_status_description, -next_payment_due_date, -principal_paid,
-         -origination_date, -interest_paid)
-
 training_predictions <- tibble(df_2014$lost_money)
+
+testing_predictions <- tibble(df_2018$lost_money)
 
 
 #####################
 # Descriptive Statistics
 
-aggregates <- df_2014 %>%
+aggregates <- df_all_years %>%
   group_by(lost_money, prosper_rating) %>%
   summarise(median_amt_borrowed = median(amount_borrowed)
             ,mean_amt_borrowed = mean(amount_borrowed)
-            ,median_rate = median(borrower_rate)
-            ,mean_rate = mean(borrower_rate)
+            # ,median_rate = median(borrower_rate)
+            # ,mean_rate = mean(borrower_rate)
             ,count = n())
 
 (aggregates <- unique(aggregates) %>%
@@ -73,14 +108,63 @@ aggregates <- df_2014 %>%
 
 
 #############################
+# Best Subset Selection
+
+num_predictors <- dim(df_all_years)[2] - 1
+
+best_subset_model <- regsubsets(lost_money ~ ., data = df_all_years)
+best_subset_summary <- summary(best_subset_model)
+
+# Create tibble which contains data from results object
+best_subset_results <- tibble(num_pred = 1:num_predictors
+                               ,rss = best_subset_summary$rss
+                               ,rsquared = best_subset_summary$rsq
+                               ,adj_rsquared = best_subset_summary$adjr2
+                               ,cp = best_subset_summary$cp
+                               ,bic = best_subset_summary$bic)
+
+# RSS
+plot1 <- best_subset_results %>% 
+  ggplot(aes(num_pred, rss)) + 
+  geom_point() +
+  geom_vline(aes(xintercept = which.min(best_subset_results$rss)), color = 'red') +
+  xlab('Number of Predictors') +
+  ylab('RSS')
+
+# ADJ R-SQUARED
+plot2 <- best_subset_results %>% 
+  ggplot(aes(num_pred, adj_rsquared)) + 
+  geom_point() +
+  geom_vline(aes(xintercept = which.max(best_subset_results$adj_rsquared)), color = 'red') +
+  xlab('Number of Predictors') +
+  ylab('Adj R-squared')
+
+# CP
+plot3 <- best_subset_results %>% 
+  ggplot(aes(num_pred, cp)) + 
+  geom_point() +
+  geom_vline(aes(xintercept = which.min(best_subset_results$cp)), color = 'red') +
+  xlab('Number of Predictors') +
+  ylab('Cp')
+
+# BIC
+plot4 <- best_subset_results %>% 
+  ggplot(aes(num_pred, bic)) + 
+  geom_point() +
+  geom_vline(aes(xintercept = which.min(best_subset_results$bic)), color = 'red') +
+  xlab('Number of Predictors') +
+  ylab('BIC')
+
+# Each of the measures comes up with vastly different number of 
+# predictors... 
+grid.arrange(plot1, plot2, plot3, plot4, nrow = 2, ncol = 2)
+
+# Selecting a model. Display coefficients
+coef(best_subset_model, 10)
+
+
+#############################
 # Linear Model
-
-# Note: Add cross-validation
-
-
-#In addition: Warning messages: ???????????????????????????????????????????????????????????/
-#  1: glm.fit: algorithm did not converge 
-#  2: glm.fit: fitted probabilities numerically 0 or 1 occurred
 
 linear_model <- glm(lost_money ~ ., data = df_2014, family = 'binomial')
 summary(linear_model)
@@ -97,6 +181,12 @@ training_predictions <- add_predictions(df_2014, linear_model, var = 'linear_pro
 
 mean(training_predictions$linear_pred_count)
 
+
+testing_predictions <- add_predictions(df_2015, linear_model, var = 'linear_prob') %>% 
+  mutate(linear_pred = ifelse(linear_prob > .5, 1, 0)
+         ,linear_pred_count = ifelse(linear_pred == lost_money, TRUE, FALSE))
+
+mean(testing_predictions$linear_pred_count)
 
 ############################
 # LDA
